6108a7ebeb15e3ee0c7e93e77ea6324b70b6bc9d
Ing. Ricardo Elizalde <ricardo.elizaldeh@un.edu.mx>
2024-07-08 20:05:05 -0600

    Restaruar Catalogode proveedores

--- PruebaEntityFrameworkCore/Controllers/RolController.cs
+++ PruebaEntityFrameworkCore/Controllers/RolController.cs
@@ -67,5 +67,90 @@ namespace PruebaEntityFrameworkCore.Controllers
 
         }
 
+
+        public IActionResult RolEdit(Guid Id)
+        {
+            var model = new RolViewModel();
+
+            var entity = _context.Roles
+            .FirstOrDefault(r => r.Id == Id.ToString());
+
+            model.Id = new Guid(entity.Id);
+            model.Name = entity.Name;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RolEdit(RolViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var entity =  this._context.Roles
+            .FirstOrDefault(r => r.Id == model.Id.ToString());
+
+            entity.Name = model.Name;
+
+            var result = await _roleManager.UpdateAsync(entity);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("RolList", "Rol");
+            }
+
+            foreach (IdentityError error in result.Errors) 
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(model);
+
+        }
+
+
+        public IActionResult RolDeleted(Guid Id)
+        {
+            var model = new RolViewModel();
+
+            var entity = _context.Roles
+            .FirstOrDefault(r => r.Id == Id.ToString());
+
+            model.Id = new Guid(entity.Id);
+            model.Name = entity.Name;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RolDeleted(RolViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var entity =  this._context.Roles
+            .FirstOrDefault(r => r.Id == model.Id.ToString());
+
+            var result = await _roleManager.DeleteAsync(entity);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("RolList", "Rol");
+            }
+
+            foreach (IdentityError error in result.Errors) 
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(model);
+
+        }
+
+
     }
 }
